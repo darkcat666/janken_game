892ce5c45d3f69952f06077f0f89eca15087d268
レオさーん <holycat666@gmail.com>
2016-04-16 06:50:55 +0000

    Refactor solve.rb

--- solve.rb
+++ solve.rb
@@ -21,60 +21,46 @@ lose = 0
 player_hand = player.split('')
 rival_hand = rival.split('')
 
-def hand_and_hand(player_hand, rival_hand)
-  win_count = 0
-  lose_count = 0
+class Solver
+  def hand_and_hand(player_hand, rival_hand)
+    win_count = 0
+    lose_count = 0
+    # 結合した手を、勝ちパターン負けパターンで分類する
+    win_pattern = ['02','25','50']
+    lose_pattern = %w(05 20 52)
 
-  player_hand.each_with_index do |p_hand, i|
-    # p "#{p_hand} vs #{rival_hand[i]}" # デバッグプリント
-    if (p_hand.to_i == 0 && rival_hand[i].to_i == 2)
-      # p "あなたは、グーで勝ちました"
-      win_count += 1
-    end
-    if (p_hand.to_i == 2 && rival_hand[i].to_i == 5)
-      # p "あなたは、チョキで勝ちました"
-      win_count += 1
-    end
-    if (p_hand.to_i == 5 && rival_hand[i].to_i == 0)
-      # p "あなたは、パーで勝ちました"
-      win_count += 1
-    end
-    if (p_hand.to_i == 0  && rival_hand[i].to_i == 5)
-      # p "あなたは、グーで負けました"
-      lose_count += 1
-    end
-    if (p_hand.to_i == 2  && rival_hand[i].to_i == 0)
-      # p "あなたは、チョキで負けました"
-      lose_count += 1
-    end
-    if (p_hand.to_i == 5  && rival_hand[i].to_i == 2)
-      # p "あなたは、パーで負けました"
-      lose_count += 1
-    end
+    player_hand.each_with_index do |p_hand, i|
+      hand = "#{p_hand}#{rival_hand[i]}"
+      if win_pattern.include?(hand)
+        win_count += 1
+      end
+      if lose_pattern.include?(hand)
+        lose_count += 1
+      end
+
+    {
+      win: win_count,
+      lose: lose_count
+    }
   end
-  # hash をつかって win, lose の数値を返す
-  {
-    win: win_count,
-    lose: lose_count
-  }
 end
 
-result = hand_and_hand(player_hand, rival_hand)
-win = result[:win]
-lose = result[:lose]
+# result = hand_and_hand(player_hand, rival_hand)
+# win = result[:win]
+# lose = result[:lose]
 
-p "win: #{win}"
-p "lose:#{lose}"
+# p "win: #{win}"
+# p "lose:#{lose}"
 #...
 # このあたりに回答のコードを書く
 #...
 
-hash = {
-  player: player,
-  rival: rival,
-  win: win,
-  lose: lose
-}
+# hash = {
+#  player: player,
+#  rival: rival,
+#  win: win,
+#  lose: lose
+# }
 
 # 実行確実になりそうになるまで おあずけ
-p judge(hash)
+# p judge(hash)
